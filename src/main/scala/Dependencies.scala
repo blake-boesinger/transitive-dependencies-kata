e7a8404946a8b1902832f4f667fa6609c098866e
Blake Boesinger <blake@boesinger.org>
2012-07-04 19:09:21 +0100

    first test passing

--- src/main/scala/Dependencies.scala
+++ src/main/scala/Dependencies.scala
@@ -1,19 +1,24 @@
 package org.boesinger
 
+import collection.mutable
+import com.detector.{Date, IpAddress}
+
 class Dependencies {
 
 
+  private var deps: mutable.HashMap[Int, List[Int]] = new mutable.HashMap[Int, List[Int]]()
 
+  def add_direct(i: Int, input: List[Int]) {
 
-  def add_direct(char : Int, input : List[Int] )  {
+    deps += ( i -> input)
 
 
 
   }
 
 
-
-  def dependencies_for( c : Int) {
+  def dependencies_for(i: Int) : List[Int] = {
+      return  deps.get(i).get
 
 
   }
